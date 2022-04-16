64fdc136633c0ebe8c97194f07905d084cd9b5c0
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:14 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/core/pages/union/UnionPageRepository.java
+++ src/main/java/com/semanticcms/core/pages/union/UnionPageRepository.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-core-pages-union - Combines multiple sets of SemanticCMS pages.
- * Copyright (C) 2017, 2018, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2017, 2018, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-core-pages-union.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.core.pages.union;
 
 import com.aoapps.collections.AoCollections;
